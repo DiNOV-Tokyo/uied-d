ef2869b5c6b8a5aa2b3b6ddb3f6a54b0abca19b4
DiNOV-Tokyo <dddd3ddd@gmail.com>
2022-03-28 15:14:03 +0900

    text-compile

---
+++ compile_text.py
@@ -0,0 +1,116 @@
+import os
+import cv2
+import json
+import sys
+import collections
+
+args = sys.argv
+# filename without extension
+filename = "br"
+filename = args[1]
+filename_img = "./data/input/" + filename + ".jpg"
+filename_json = "./data/output/merge/" + filename + ".json"
+out_dir = "./data/output/merge/" + filename
+filename_html = out_dir + "/" + filename + ".html"
+filename_css = out_dir + "/" + filename + ".css"
+pic_dir = out_dir + "/" + filename + "_pic"
+rel_pic_dir = "./" + filename + "_pic"
+
+if not os.path.exists(out_dir):
+    # ディレクトリが存在しない場合、ディレクトリを作成する
+    os.makedirs(out_dir)
+
+if not os.path.exists(pic_dir):
+    # ディレクトリが存在しない場合、ディレクトリを作成する
+    os.makedirs(pic_dir)
+
+with open(filename_json, "r") as f:
+    jon_dat = json.load(f)
+
+a = json.dumps(jon_dat)
+element_num = a.count('"id":') -1
+
+# 背景色を読み取る
+pos = (20, 20)
+img = cv2.imread(filename_img, cv2.IMREAD_UNCHANGED)
+color = list(img[pos])
+color_str = "#" + format(color[2], 'x').zfill(2) + format(color[1], 'x').zfill(2) + format(color[0], 'x').zfill(2)
+print("color=" + str(color_str))
+print(element_num)
+with open(filename_html, "w") as f3:
+    f3.writelines('<!DOCTYPE html>\n')
+    f3.writelines('<html lang="ja">\n')
+    f3.writelines('<head><meta charset="utf-8">\n')
+    f3.writelines('<title>タイトル</title>\n')
+    f3.writelines('<link rel="stylesheet" href="' + filename + '.css">\n')
+    f3.writelines('</head><body bgcolor="' + color_str + '">\n')
+
+with open(filename_css, "w") as f2:
+    f2.writelines(".pbox{\n")
+    f2.writelines("display: flex;\n")
+    f2.writelines("border: solid 1px transparent;\n")
+    f2.writelines("padding: 5px;\n")
+    f2.writelines("}\n")
+
+next_div = False
+pre_row_min = 0
+pre_row_max = jon_dat["compos"][0]["position"]["row_max"]
+div_list = []
+
+for i in range(element_num):
+    x_c = int((int(jon_dat["compos"][i]["position"]["column_max"]) + int(jon_dat["compos"][i]["position"]["column_min"])) / 2 )
+    y_c = int((int(jon_dat["compos"][i]["position"]["row_max"]) + int(jon_dat["compos"][i]["position"]["row_min"])) / 2 ) 
+
+    s_width = str(jon_dat["compos"][i]["width"])
+    s_height = str(jon_dat["compos"][i]["height"])
+
+    col_left = jon_dat["compos"][i]["position"]["column_min"]
+    col_right = jon_dat["compos"][i]["position"]["column_max"]
+    row_top = jon_dat["compos"][i]["position"]["row_min"]
+    row_bottom = jon_dat["compos"][i]["position"]["row_max"]
+    
+    img = cv2.imread(filename_img)
+    img1 = img[row_top : row_bottom, col_left : col_right]
+    cv2.imwrite(pic_dir + "/out_sample1" + str(i) + ".jpg", img1)
+
+
+# 連続した同じサイズのテキストの番号(id)のリスト
+txt_num_list = []
+
+# JSONファイル内の最初に出てくるテキストかどうか
+txt_flg = True
+
+
+for i in range(element_num):
+
+    next_min = jon_dat["compos"][i+1]["position"]["row_min"]
+    now_min = jon_dat["compos"][i]["position"]["row_min"]
+    now_height = jon_dat["compos"][i]["height"]
+
+    # まずは、classがテキストの場合のみを考慮する。
+    if jon_dat["compos"][i]["class"] == "Text":
+        print("txt")
+        if txt_flg:
+            pre_txt_height = jon_dat["compos"][i]["height"]
+            txt_flg = False
+            print("pre_txt" + str(pre_txt_height))
+
+        txt_height = jon_dat["compos"][i]["height"]
+        # ひとつ前のテキストと同じサイズなら、リストに番号を加える。
+        if abs(pre_txt_height - txt_height) < 2:
+            txt_num_list.append(i)
+
+        # ひとつ前のテキストと異なるサイズなら、１つ前までのテキストをhtmlファイルに書き出し。新たにリストを作る。
+        else:
+            for j in txt_num_list:
+                with open(filename_html, "a") as f3:
+                    f3.writelines('<div>\n')
+                    f3.writelines(jon_dat["compos"][j]["text_content"] + '\n')
+                    f3.writelines('<div>\n')
+                    print(jon_dat["compos"][j]["text_content"])
+
+            txt_num_list = []
+            txt_num_list.append(i)
+            pre_txt_height = jon_dat["compos"][i]["height"]
+
+
